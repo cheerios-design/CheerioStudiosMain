ec0b7962d4a2ce34eb86a2bc767a9b99bdb29493
Sam Daramroei <31776791+cheerios-design@users.noreply.github.com>
2025-11-23 00:24:29 +0300

    feat: Add Vite configuration for React project with path aliases and server settings

--- cheerio-studio/vite.config.ts
+++ cheerio-studio/vite.config.ts
@@ -5,9 +5,15 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  }, // <--- Make sure to close the 'resolve' block here with a comma
+
+  // The server block goes here (outside of resolve)
+  server: {
+    allowedHosts: true,
   },
 })
